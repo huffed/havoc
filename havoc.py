358f950036439b8a048137d314453790251c2926
Jack Jones <jacksopomc@gmail.com>
2023-09-01 22:11:32 +0100

    login page errors and stuff

--- havoc.py
+++ havoc.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_limiter import Limiter
 from flask_caching import Cache
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
-from utils.sql_connect import mysql_connection
+from utils.sql_connect import mysql_uri
 from argon2 import PasswordHasher, Type
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "havoc"
+
+"""
+MySQL DB
+"""
+app.config["SQLALCHEMY_DATABASE_URI"] = mysql_uri
+mysql_db = SQLAlchemy(app)
+
 limiter = Limiter(
     lambda: str(request.remote_addr),
     app=app,
@@ -33,12 +42,19 @@ def index():
 def login():
     if request.method == "POST":
         # add encryption to password form and integrate with a database
-        select_query = text("select username, password from users where username = :username")
-        valid_check = mysql_connection.execute(select_query, parameters={"username": request.form.get("u")})
-        print(valid_check)
-        return redirect('#')
+        select_query = text("""
+                            select username, password from users where username = :username
+                            """)
+        valid_check = mysql_db.session.execute(
+            select_query, {"username": request.form.get("username")}).fetchone()
+        try:
+            hasher.verify(valid_check[1], request.form.get("password"))
+            return redirect(url_for("dashboard"))
+        except:
+            error = "Username or password not found."
+            return render_template("login.html", error=error)
     else:
-        return render_template('login.html')
+        return render_template("login.html")
 
 
 @app.route('/register', methods=['POST', 'GET'])
@@ -52,27 +68,30 @@ def register():
             return render_template('register.html', error_message="Username must be at least 3 characters long.")
 
         invite_key = request.form.get('invite key')
-        select_query = text("")
+        select_query = text("""
+                            """)
 
-        insert_statement = text(
-            "INSERT INTO users (username, password, invite) VALUES (:username, :password, :invite)")
+        insert_statement = text("""
+                                INSERT INTO users (username, password, invite) VALUES (:username, :password, :invite)
+                                """)
         data = {
-            "username": request.form.get("u"),
-            "password": hasher.hash(request.form.get("p")),
-            "invite": request.form.get("k")
+            "username": request.form.get("username"),
+            "password": hasher.hash(request.form.get("password")),
+            "invite": request.form.get("invite key")
         }
         # connection.execute(insert_statement, parameters=data)
         # connection.commit()
         return redirect('/dashboard')
     else:
-        return render_template('register.html', error_message=None)
+        return render_template('register.html')
 
 
 @app.route('/check_availability', methods=['GET'])
 @limiter.limit(limit_value="500 per hour")
 def check_availability():
-    select_query = text(
-        "select username from users where username = :username")
+    select_query = text("""
+                        select username from users where username = :username
+                        """)
 
     # add different process for when its coming from register or login
     # check = connection.execute(select_query, parameters={username: request.form.get('username')}
--- static/js/script.js
+++ static/js/script.js
@@ -1,44 +1,48 @@
-$(document).ready(function() {
-    var timeout;
+$(document).ready(function () {
+  var timeout;
 
-    function validateinput() {
-        var input = $("input[name='username']");
-        var username = input.val();
-        var error = $("#error-message");
+  function validateinput() {
+    var input = $(".register-container input[name='username']");
+    var username = input.val();
+    var error = $("#error-message");
 
-        if (username.length === 0) {
-            error.hide();
-            input.removeClass("input-error");
-        } else if (username.length <= 2) {
-            clearTimeout(timeout);
-                timeout = setTimeout(function() {
-                error.text("Username must be at least 3 characters long");
-                error.show();
-                input.addClass("input-error");
-            }, 300);
-        } else {
-            error.hide();
-            input.removeClass("input-error");
+    error.hide();
 
-            clearTimeout(timeout);
-            timeout = setTimeout(function() {
-                checkUsernameAvailability(username);
-            }, 500);
-        }
+    if (username != null) {
+      if (username.length === 0) {
+        error.hide();
+        input.removeClass("input-error");
+      } else if (username.length <= 2) {
+        clearTimeout(timeout);
+        timeout = setTimeout(function () {
+          error.text("Username must be at least 3 characters long");
+          error.show();
+          input.addClass("input-error");
+        }, 300);
+      } else {
+        error.hide();
+        input.removeClass("input-error");
+
+        clearTimeout(timeout);
+        timeout = setTimeout(function () {
+          checkUsernameAvailability(username);
+        }, 500);
+      }
     }
+  }
 
-    function checkUsernameAvailability(username) {
-        $.ajax({
-            type: "GET",
-            url: "/check_availability",
-            data: { username: username },
-            success: function(response) {
-                // Handle the response if needed
-            }
-        });
-    }
-
-    ['input', 'blur'].forEach(function(event){
-        window.addEventListener(event,validateinput);
+  function checkUsernameAvailability(username) {
+    $.ajax({
+      type: "GET",
+      url: "/check_availability",
+      data: { username: username },
+      success: function (response) {
+        // Handle the response if needed
+      },
     });
+  }
+
+  ["input", "blur"].forEach(function (event) {
+    window.addEventListener(event, validateinput);
+  });
 });

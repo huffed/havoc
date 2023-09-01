43e1714451ed2ac4b3ff94a3dd3154255b92dd2c
Jack Jones <jacksopomc@gmail.com>
2023-09-01 23:09:20 +0100

    Revert "login page errors and stuff"
    This reverts commit 358f950036439b8a048137d314453790251c2926.

--- static/js/script.js
+++ static/js/script.js
@@ -1,52 +1,47 @@
-$(document).ready(function () {
-  var timeout;
+$(document).ready(function() {
+    var timeout;
 
   function validateinput() {
     var input = $(".register-container input[name='username']");
     var username = input.val();
     var error = $("#error-message");
-    var submit = $("input[type='submit' i]");
 
-    error.text("");
-    submit.css("margin-top", "20px");
+    error.hide();
 
     if (username != null) {
       if (username.length === 0) {
-        error.text("");
-        submit.css("margin-top", "20px");
+        error.hide();
         input.removeClass("input-error");
       } else if (username.length <= 2) {
         clearTimeout(timeout);
         timeout = setTimeout(function () {
           error.text("Username must be at least 3 characters long");
-          submit.css("padding-bottom", "4px");
+          error.show();
           input.addClass("input-error");
         }, 300);
       } else {
-        error.text("");
-        submit.css("margin-top", "20px");
+        error.hide();
         input.removeClass("input-error");
 
-        clearTimeout(timeout);
-        timeout = setTimeout(function () {
-          checkUsernameAvailability(username);
-        }, 500);
-      }
+            clearTimeout(timeout);
+            timeout = setTimeout(function() {
+                checkUsernameAvailability(username);
+            }, 500);
+        }
     }
-  }
 
-  function checkUsernameAvailability(username) {
-    $.ajax({
-      type: "GET",
-      url: "/check_availability",
-      data: { username: username },
-      success: function (response) {
-        // Handle the response if needed
-      },
+    function checkUsernameAvailability(username) {
+        $.ajax({
+            type: "GET",
+            url: "/check_availability",
+            data: { username: username },
+            success: function(response) {
+                // Handle the response if needed
+            }
+        });
+    }
+
+    ['input', 'blur'].forEach(function(event){
+        window.addEventListener(event,validateinput);
     });
-  }
-
-  ["input", "blur"].forEach(function (event) {
-    window.addEventListener(event, validateinput);
-  });
 });
--- utils/sql_connect.py
+++ utils/sql_connect.py
@@ -1,4 +1,5 @@
 import json
+from sqlalchemy import create_engine
 
 with open('config/config.json') as f:
     connection = json.load(f)
@@ -9,4 +10,6 @@ databases = connection['databases']
 MYSQL Database
 """
 mysql_info = databases['mysql']
-mysql_uri = f"mysql+mysqlconnector://{mysql_info['username']}:{mysql_info['password']}@{mysql_info['hostname']}:{mysql_info['port']}/{mysql_info['schema']}"
+mysql_url = f"mysql+mysqlconnector://{mysql_info['username']}:{mysql_info['password']}@{mysql_info['hostname']}:{mysql_info['port']}/{mysql_info['schema']}"
+mysql_engine = create_engine(mysql_url)
+mysql_connection = mysql_engine.connect()

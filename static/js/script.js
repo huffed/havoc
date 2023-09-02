250264a4509274a67fc4aec4358323f444ec89e2
Jack Jones <jacksopomc@gmail.com>
2023-09-02 02:01:25 +0100

    finally some decent css, need to work on cleaning it up and same with the js

--- static/js/script.js
+++ static/js/script.js
@@ -1,26 +1,41 @@
 $(document).ready(function () {
+  function ready() {
+    var error = $("#error-message");
+    var submit = $(".submit-button");
+
+    if (error.text() != null) {
+      submit.css("margin-top", "0");
+    } else {
+      submit.css("margin-top", "40px");
+    }
+  }
   var timeout;
 
   function validateinput() {
     var input = $(".register-container input[name='username']");
     var username = input.val();
     var error = $("#error-message");
+    var submit = $(".submit-button");
 
-    error.hide();
+    if (error.text() == "Username or password incorrect.") {
+      error.text("");
+      submit.css("margin-top", "40px");
+    }
 
     if (username != null) {
       if (username.length === 0) {
-        error.hide();
+        error.text("");
+        submit.css("margin-top", "40px");
         input.removeClass("input-error");
       } else if (username.length <= 2) {
         clearTimeout(timeout);
         timeout = setTimeout(function () {
           error.text("Username must be at least 3 characters long");
-          error.show();
+          submit.css("margin-top", "0");
           input.addClass("input-error");
         }, 300);
       } else {
-        error.hide();
+        error.text("");
         input.removeClass("input-error");
 
         clearTimeout(timeout);
@@ -45,4 +60,6 @@ $(document).ready(function () {
   ["input", "blur"].forEach(function (event) {
     window.addEventListener(event, validateinput);
   });
+
+  window.addEventListener("load", ready);
 });

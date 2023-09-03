13b432b696462aa5bbe9cf182e5507b94f8ed38c
geniusman420 <jacksopomc@gmail.com>
2023-09-03 20:32:24 +0100

    moved these files to v1 and making new master

--- create-environment.bat
+++
@@ -1,5 +0,0 @@
-call conda create -y -n Havoc python=3.11.4
-
-call conda activate Havoc
-
-call pip install --no-input -r requirements.txt
--- create-environment.sh
+++
@@ -1,5 +0,0 @@
-conda create -y -n Havoc python=3.11.4
-
-conda activate Havoc
-
-pip install --no-input -r requirements.txt

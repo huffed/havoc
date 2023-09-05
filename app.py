e2fb869391b8fc35339734488d482d7e746ee701
geniusman420 <jacksopomc@gmail.com>
2023-09-05 03:43:24 +0100

    lil thing before i go bed

--- app.py
+++ app.py
@@ -87,12 +87,11 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
 
-        if user:
-            if argon2.check_password_hash(user.password, form.password.data):
-                login_user(user)
-                return redirect(url_for("dashboard"))
-            else:
-                return render_template("login.html", form=form)
+        if user and argon2.check_password_hash(user.password, form.password.data):
+            login_user(user)
+            return redirect(url_for("dashboard"))
+        else:
+            return render_template("login.html", form=form)
 
     return render_template("login.html", form=form)
 

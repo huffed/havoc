8fc030622922590132f7f94fc5c58f60f1284489
Jack Jones <jacksopomc@gmail.com>
2023-09-15 14:54:43 +0100

    removing sensitive stuff

--- config.py
+++
@@ -1,7 +0,0 @@
-mysql = {
-    "username": "jack",
-    "password": "Cr1m1n4ls",
-    "host": "109.154.15.11",
-    "port": "3306",
-    "schema": "admin"
-}
--- create-environment.bat
+++
@@ -1,5 +0,0 @@
-conda create -y -n havoc python=3.11.4
-
-conda activate havoc
-
-pip install --no-input -r requirements.txt
